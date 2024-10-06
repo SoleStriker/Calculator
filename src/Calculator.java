bc509f23618970aa68764b36f0b5568b929cafa1
Sole <114538195+mahhanakbaran@users.noreply.github.com>
2024-10-05 23:53:14 -0500

    Added new gui icon and text box

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,11 +11,11 @@ public class Calculator implements ActionListener {
     JButton addButton, subtractButton, multiplyButton, divideButton;
     JButton clearButton, clearAllButton;
     JButton decButton, equalButton, delButton, clrButton;
-
+    JPanel panel;
     Font myFont = new Font("Serif", Font.BOLD, 30);
 
     double num1 = 0,num2 = 0, result = 0;
-
+    char operator;
 
     Calculator() {
         frame = new JFrame("Calculator");
@@ -23,16 +23,25 @@ public class Calculator implements ActionListener {
         frame.setSize(420, 550);
         frame.setLayout(null);
 
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
+        frame.add(textField);
+
+        ImageIcon img = new ImageIcon("assets/images/CalculatorIcon.png");
+        frame.setIconImage(img.getImage());
+//      textField.setEditable(false);
+
 
         frame.setVisible(true);
-
     }
+
     public static void main(String[] args) {
         Calculator calc = new Calculator();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-                        
     }
 }
+
